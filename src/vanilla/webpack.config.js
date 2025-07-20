304588bb64f9e6b74f956c0aac925541d80e9cf1
skarahajimaruore <tmh44132436@gmail.com>
2025-07-20 13:02:26 +0900

    Fix

--- src/vanilla/webpack.config.js
+++ src/vanilla/webpack.config.js
@@ -1,63 +1,35 @@
 // webpack.config.js
 const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const Dotenv = require("dotenv-webpack");
 
-const env = process.env.NODE_ENV || "production";
-
 module.exports = {
-  mode: env,
-
-  entry: path.resolve(__dirname, "src/vanilla/index.js"),
-
+  entry: "./src/vanilla/index.js", // ←ここを修正
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
-    publicPath: "/",
+    filename: "bundle.js",
   },
-
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "src/vanilla"),
-      watch: true,
-    },
-    port: 3000,
-  },
-
-  resolve: {
-    fallback: {
-      crypto: false,
-    },
-  },
-
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
-        use: [
-          env === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
-
   plugins: [
-    new Dotenv({ systemvars: true }),
-    new webpack.HotModuleReplacementPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/vanilla/index.html"),
+      template: "./src/vanilla/index.html", // ← これも合わせて修正
     }),
+    new MiniCssExtractPlugin(),
+    new Dotenv(),
   ],
 };
